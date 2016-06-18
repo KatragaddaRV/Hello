57ec12adc1a5eea63f323996f6ecbbb05a2dd8a6
Ramana Katragadda <ramana@katragadda.com>
2016-06-18 18:54:38 -0500

    Added SConstruct to build with scons utility.

--- Hello.cpp
+++ Hello.cpp
@@ -5,22 +5,21 @@
 
 int main(int argc, char **argv)
 {
-    Hello message;
 
-    time_t _tm =time(NULL );
+    time_t _tm= time(NULL);
+    struct tm * curtime= localtime(&_tm);
 
-    struct tm * curtime = localtime ( &_tm );
     string currentTimeString(asctime(curtime));
-    
 
+    Hello message;
     const char *myStrings[]= 
-    {    "Hello, world.",
-         "-- Testing Git Repo.",
-         "   -- Concludes Testing.",
-         "         -- Added Support for SCONS."
-    };
+               {    "Hello, world.",
+                    "-- Testing Git Repo.",
+                    "   -- Concludes Testing.",
+                    "         -- Repo can now be built with scons"
+               };
 
-    vector <string>  tStringVector(myStrings, myStrings+5);
+    vector <string>  tStringVector(myStrings, myStrings+(sizeof(myStrings)/sizeof(char *)));
     vector<string>::const_iterator it;
 
     for( it= tStringVector.begin(); it != tStringVector.end(); ++it)
@@ -28,7 +27,7 @@ int main(int argc, char **argv)
     }
 
     message.print();
-    cout << "The current date/time is:" << currentTimeString << endl;
+    cout << "The current date/time is: " << currentTimeString << endl;
 
     return ( 1 );
 }
