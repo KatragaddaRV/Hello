b643c53a47be8887ba7190982a5718f306460b38
Ramana <ramana@katragadda.com>
2016-06-18 19:10:03 -0500

    Final version with DateTime Stamp and scons build.

--- Hello.cpp
+++ Hello.cpp
@@ -1,17 +1,14 @@
 // Hello.cpp : Defines the entry point for the console application.
-//
 #include "Hello.h"
 #include <ctime>
 
 int main(int argc, char **argv)
 {
-
     time_t _tm= time(NULL);
     struct tm * curtime= localtime(&_tm);
 
     string currentTimeString(asctime(curtime));
 
-    Hello message;
     const char *myStrings[]= 
                {    "Hello, world.",
                     "-- Testing Git Repo.",
@@ -22,11 +19,12 @@ int main(int argc, char **argv)
     vector <string>  tStringVector(myStrings, myStrings+(sizeof(myStrings)/sizeof(char *)));
     vector<string>::const_iterator it;
 
+    Hello message;
     for( it= tStringVector.begin(); it != tStringVector.end(); ++it)
     {   message.add(*it);
     }
-
     message.print();
+
     cout << "The current date/time is: " << currentTimeString << endl;
 
     return ( 1 );
